5fb86b5530e543600c3e514b1bf19b3e568cae70
zschur <zachschur@gmail.com>
2016-11-21 20:39:20 -0500

    before thanksgiving

--- csi185website/myapp/views.py
+++ csi185website/myapp/views.py
@@ -3,5 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 
 def all(request):
-    print(request.path_info.split('/').pop())
-    return render(request,request.path_info.split('/').pop())
+    lst = request.path_info.split('/')
+    url = ""
+    while lst [len(lst)-1]!='':
+    	url = lst.pop()+'/'+url
+    return render(request,url)
